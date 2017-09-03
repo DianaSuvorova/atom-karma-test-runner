f3dd5915c69082e427f55fb9aeb324872726cd89
Diana Suvorova <dsuvorova@DSuvorova-196.local>
2017-09-02 17:02:34 -0700

    specs -> spec

--- spec/getContext-spec.js
+++ spec/getContext-spec.js
@@ -8,10 +8,4 @@ describe('closestPackage', () => {
     const packageForlder = path.dirname(__dirname);
     expect(closestPackage(__dirname)).toEqual(packageForlder);
   });
-
-  it('returns enclosinf npm package for test package spec', () => {
-    const testpackage = path.join(__dirname, 'testpackage');
-    const spec = path.join(testpackage,  'specs', 'spec.js');
-    expect(closestPackage(spec)).toEqual(testpackage);
-  })
 });
